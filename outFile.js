859499b0ca88909572a1f21d190a3a74bec9ba36
Luke <lukecui@aijiatui.com>
2019-07-01 18:07:07 +0800

    add outFile

---
+++ outFile.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+
+module.exports = async (srcBaseDir = `${__dirname}/node_modules/skybase-stat/`) => {
+  let obj = {
+    'router': {
+      'skyapi': {
+        'stat.js': './router/skyapi/stat.js'
+      }
+    },
+    'service': {
+      'skyapi': {
+        'stat.js': './service/skyapi/stat.js'
+      }
+    },
+    'model': {
+      'api': {
+        'skyapi': {
+          'stat.js': './model/api/skyapi/stat.js'
+        }
+      },
+      'htmlOut.js': './model/htmlOut.js',
+      'rts': {
+        'index.js': './model/rts/index.js',
+        'lua': {
+          'avg.lua': './model/rts/lua/avg.lua',
+          'max.lua': './model/rts/lua/max.lua',
+          'min.lua': './model/rts/lua/min.lua',
+          'update_pf.lua': './model/rts/lua/update_pf.lua',
+        },
+        'util': {
+          'util.js': './model/rts/util/util.js'
+        }
+      }
+    }
+  }
+  try {
+    await outPutFile('.', '', obj, srcBaseDir)
+  } catch (e) {
+    console.error(e.stack)
+  }
+}
+
+async function outPutFile(dir, key, obj, srcBaseDir) {
+  if (typeof obj == 'string') {
+    // console.log(`创建目录  ${dir}`)
+    await fs.mkdirSync(dir, { recursive: true })
+    // console.log(`原：${srcBaseDir}${obj}  目的：${dir}/${key}`)
+    await fs.copyFileSync(`${srcBaseDir}${obj}`, `${dir}/${key}`);
+    return
+  }
+  for (let k in obj) {
+    outPutFile(key === '' ? '.' : `${dir}/${key}`, k, obj[k], srcBaseDir)
+  }
+}
+
